87e1f0d7fdf09c72936a64d1b169c19071191fdb
Burt Hughes <normanthompson@163.com>
2019-03-05 18:36:56 +0800

    1. add test suite.

---
+++ src/test/java/com/penguin/rpn/Test.java
@@ -0,0 +1,15 @@
+package com.penguin.rpn;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * @Auther: Burt Hughes
+ * @Date: 2019/3/4 22:46
+ * @Description: test RpnCalculator.
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ComponentsTest.class, LogicFunctionTest.class, MemoryTest.class, RpnCalculatorTest.class})
+public class Test {
+    
+}
